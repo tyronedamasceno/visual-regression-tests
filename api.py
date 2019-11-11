6b0cbf0f74988b9d5f3cede641843af3aca77ac4
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-11-11 14:45:13 -0300

    receive port from heroku

--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify
 
 from visual_regression import VisualRegression
@@ -32,5 +34,6 @@ def test_regression():
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 

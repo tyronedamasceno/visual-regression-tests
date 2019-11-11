6f1f8dc14644bf4c6235b173d8618aaf2707b8ad
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-11-11 16:39:53 -0300

    Add bucket info to env vars

--- aws_handler.py
+++ aws_handler.py
@@ -4,6 +4,8 @@ import boto3
 
 access_key_id = os.environ.get('access_key_id')
 secret_access_key = os.environ.get('secret_access_key')
+bucket_name = os.environ.get('bucket_name')
+bucket_location = os.environ.get('bucket_location')
 
 s3_client = boto3.client(
     's3',
@@ -12,6 +14,6 @@ s3_client = boto3.client(
 )
 
 def upload_file(filename):
-    s3_client.upload_file(filename, 'visual-regression-tests', filename)
-    return f'https://visual-regression-tests.s3-sa-east-1.amazonaws.com/{filename}'
+    s3_client.upload_file(filename, bucket_name, filename)
+    return f'https://{bucket_name}.{bucket_location}.amazonaws.com/{filename}'
 
